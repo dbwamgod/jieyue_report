6c1b340414253ef6991778eae58fa4573e247580
dengbowen <bowendeng@jieyuechina.com>
2018-11-08 14:11:00 +0800

    修改tabs样式

--- src/components/aside.vue
+++ src/components/aside.vue
@@ -3,17 +3,16 @@
     <el-row style="width:100%;display: -webkit-box;display: flex;margin-top:50px;" :style="{marginTop:this.hideheaderaside?50+'px':'0'}">
       <!--侧边栏-->
       <el-col :span="2" style="height:100%;position: fixed;z-index: 100;-box-flex:-webkit 1;flex:1;" class="aside" v-show="this.hideheaderaside">
-                <!-- <div class="baobiaogongneng"></div> -->
+        <!-- <div class="baobiaogongneng"></div> -->
         <!--搜索框和收藏-->
         <div style="height: 100%;" v-show="val">
-          <el-menu :router="true" :default-active="defaultActive+''" :unique-opened="true" style="height:100%;" :style="{width:ths+'px'}"
-            class="zyside">
+          <el-menu :router="true" :default-active="defaultActive+''" :unique-opened="true" style="height:100%;" :style="{width:ths+'px'}" class="zyside">
             <!--第一层-->
             <el-submenu v-for="(item,index) in this.ArrayData" :index="item.id +''" :unique-opened="true" :key="index">
-              <template slot="title"><i :class=[item.icon]></i> {{item.name}}</template>
+              <template slot="title">
+                <i :class=[item.icon]></i> {{item.name}}</template>
               <!--第二层-->
-              <el-submenu v-for="(itema,index) in item.children" :index="itema.id +''" v-if="itema.children != undefined"
-                :key="index">
+              <el-submenu v-for="(itema,index) in item.children" :index="itema.id +''" v-if="itema.children != undefined" :key="index">
                 <!--第三层-->
                 <template slot="title" v-show='itema.name'>{{itema.name}}</template>
                 <el-menu-item-group v-for="(itemb,index) in itema.children" :index="itemb.id +''" :key="index">
@@ -21,14 +20,15 @@
                 </el-menu-item-group>
               </el-submenu>
               <!--第二层-->
-              <el-menu-item v-if="itema.children == undefined " :index="itema.id+''" @click="gotoPath(itema.url,itema.id) ;addTab(editableTabsValue2,itema.url,itema.name,itema.id)"
-                v-for="(itema,index) in item.children" :key="index">{{itema.name}}</el-menu-item>
+              <el-menu-item v-if="itema.children == undefined " :index="itema.id+''" @click="gotoPath(itema.url,itema.id) ;addTab(editableTabsValue2,itema.url,itema.name,itema.id)" v-for="(itema,index) in item.children" :key="index">{{itema.name}}</el-menu-item>
             </el-submenu>
           </el-menu>
         </div>
         <div v-show="!val" style="width:50px;height:100%;background:#3c404c;">
           <ul>
-            <li v-for="(item,index) in this.ArrayData" :key="index" style="padding-top:12px;"><i :class=[item.icon]></i></li>
+            <li v-for="(item,index) in this.ArrayData" :key="index" style="padding-top:12px;">
+              <i :class=[item.icon]></i>
+            </li>
           </ul>
         </div>
 
@@ -36,36 +36,33 @@
 
       <!--页面的内容-->
       <!-- <div v-show="this.hideheaderaside" style="width:100%;height:100%;"> -->
-        <el-col :span="22" style="-webkit-box-flex: 1;width:87%;height:100%;flex:1;position: relative;background: #f1f2f6"
-          :style="this.hideheaderaside?(this.val?{marginLeft:'160px'}:{marginLeft:'50px'}) :({marginLeft:0+'px'})">
-          <div style="width: 2px;height: 100%;background:rgba(0,0,0,.1);position: absolute;" id="DetectionHeight"></div>
+      <el-col :span="22" style="-webkit-box-flex: 1;width:87%;height:100%;flex:1;position: relative;background: #f1f2f6" :style="this.hideheaderaside?(this.val?{marginLeft:'160px'}:{marginLeft:'50px'}) :({marginLeft:0+'px'})">
+        <div style="width: 2px;height: 100%;background:rgba(0,0,0,.1);position: absolute;" id="DetectionHeight"></div>
 
-          <div style="cursor: pointer;z-index:999" @click="hideV" v-show="this.hideheaderaside" :class="{hv:hl,hh:!hl}" :style="{left:ths+'px'}" v-if="val">
-            <img :src="shouqizuocedaohang" alt="">
-          </div> 
+        <div style="cursor: pointer;z-index:999" @click="hideV" v-show="this.hideheaderaside" :class="{hv:hl,hh:!hl}" :style="{left:ths+'px'}" v-if="val">
+          <img :src="shouqizuocedaohang" alt="">
+        </div>
 
-          <div style="z-index: 999;cursor: pointer" @click="hideV" v-show="this.hideheaderaside" :class="{hv:hl,hh:!hl}" :style="{ left:this.val?ths+'px':'50px'}"
-            v-else>
-            <img :src="shouqizuocedaohang" alt="">
-          </div> 
+        <div style="z-index: 999;cursor: pointer" @click="hideV" v-show="this.hideheaderaside" :class="{hv:hl,hh:!hl}" :style="{ left:this.val?ths+'px':'50px'}" v-else>
+          <img :src="shouqizuocedaohang" alt="">
+        </div>
 
-          <!--整体导航-->
-          <div style="margin: 16px;" class="OverallNavigation">
-            <!--图标-->
+        <!--整体导航-->
+        <div style="margin: 16px;" class="OverallNavigation">
+          <!--图标-->
 
-            <!-- <span class="el-dropdown-link" style="float: left;padding:3px 16px;height: 26px;display: inline-block;background: #3C404C;border-radius: 4px 4px 0 0;"
+          <!-- <span class="el-dropdown-link" style="float: left;padding:3px 16px;height: 26px;display: inline-block;background: #3C404C;border-radius: 4px 4px 0 0;"
               @click="buttonMainD">
               <img :src="fanhuishouye" alt="" style="width: 20px;height: 20px">
             </span> -->
 
-            <!--触发器-->
-            <el-tabs id="editableTabs" v-model="editableTabsValue2" type="card" @tab-click="tabsn" closable @tab-remove="removeTab"
-              style="z-index: 999;height: 32px;background: #ffffff;" v-show="Deletenavigationbar">
-              <el-tab-pane v-for="option in $store.state.editableTabs2" :label="option.title" :name="option.content" :key="option.name"></el-tab-pane>
-            </el-tabs>
-          </div>
-          <router-view style="margin: 16px 30px;"></router-view>
-        </el-col>
+          <!--触发器-->
+          <el-tabs id="editableTabs" v-model="editableTabsValue2" type="card" @tab-click="tabsn" closable @tab-remove="removeTab" style="z-index: 999;height: 32px;" v-if="Deletenavigationbar">
+            <el-tab-pane v-for="option in $store.state.editableTabs2" :label="option.title" :name="option.content" :key="option.name"></el-tab-pane>
+          </el-tabs>
+        </div>
+        <router-view style="margin: 16px 30px;"></router-view>
+      </el-col>
       <!-- </div> -->
     </el-row>
   </div>
@@ -100,7 +97,7 @@ export default {
       fanhuishouye: require("../assets/images/icon-fanhuishouye.png"), //小房子 icon-shouqizuocedaohang
       shouqizuocedaohang: require("../assets/images/icon-shouqizuocedaohang.png"), // icon-sousuo.png
       icon: [{ name: 1 }, { name: 1 }, { name: 1 }, { name: 1 }, { name: 1 }],
-      Deletenavigationbar: false
+      Deletenavigationbar: !!this.$route.meta.isShowTabs
     };
   },
   computed: {
@@ -113,6 +110,10 @@ export default {
     },
     CollectionNameLis: function() {
       this.CollectionNameLislength = this.CollectionNameLis.length;
+    },
+    $route(to, from) {
+      this.editableTabsValue2 = to.name;
+      this.Deletenavigationbar = !!to.meta.isShowTabs;
     }
   },
   beforeUpdate() {
@@ -245,7 +246,6 @@ export default {
           this.editableTabsValue2 = newTabName;
 
           this.$store.commit("SAVE_EDITABLETABS2", this.editableTabs2);
-          this.Deletenavigationbar = true;
         }
       }
     }, //点击侧边栏 添加在导航栏上
@@ -264,7 +264,6 @@ export default {
       }
       this.editableTabsValue2 = activeName;
       if (this.$store.state.editableTabs2.length == 1) {
-        this.Deletenavigationbar = false;
         this.editableTabs2 = tabs.filter(tab => tab.content !== targetName);
         this.tabName = [];
         this.$store.commit("SAVE_EDITABLETABS2", this.editableTabs2);
@@ -404,5 +403,27 @@ export default {
   height: 18px;
   background: url(../assets/images/icon-baobiaogongneng.png) no-repeat;
   display: inline-block;
+}
+#editableTabs > .el-tabs__header {
+  border: none;
+}
+#editableTabs >.el-tabs__header .el-tabs__nav{
+   border-radius: 15px 15px 0 0;
+}
+#editableTabs > .el-tabs__header .el-tabs__item.is-active {
+  border-bottom-color: transparent;
+}
+#editableTabs > .el-tabs__header .el-tabs__item {
+  border-bottom-color: transparent;
+  background: #8b8b8b;
+  color: #fff;
+  font-size: 16px;
+  border-radius: 15px 15px 0 0;
+}
+#editableTabs>.el-tabs__header .el-tabs__item .el-icon-close{
+  width:22px;
+  font-size: 20px;
+  height:22px;
+  line-height: 22px;
 }
 </style>
